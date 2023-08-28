3168474ba6582c19fc0565505b7dee04162290cc
Eric L <126723001+elavallee13@users.noreply.github.com>
2023-08-28 14:24:41 -0500

    Removed files from Develop folder into main

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,21 +1,32 @@
 import { openDB } from 'idb';
 
-const initdb = async () =>
-  openDB('jate', 1, {
-    upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
-    },
-  });
+const DATABASE_NAME = 'jate';
+const DATABASE_VERSION = 1;
+const OBJECT_STORE_NAME = 'content';
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+const dbPromise = openDB(DATABASE_NAME, DATABASE_VERSION, {
+  upgrade(db) {
+    if (!db.objectStoreNames.contains(OBJECT_STORE_NAME)) {
+      const store = db.createObjectStore(OBJECT_STORE_NAME, {
+        keyPath: 'id',
+        autoIncrement: true,
+      });
+    }
+  },
+});
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const db = await dbPromise;
+  const tx = db.transaction(OBJECT_STORE_NAME, 'readwrite');
+  const store = tx.objectStore(OBJECT_STORE_NAME);
+  await store.add({ content });
+};
 
-initdb();
+export const getDb = async () => {
+  const db = await dbPromise;
+  const tx = db.transaction(OBJECT_STORE_NAME, 'readonly');
+  const store = tx.objectStore(OBJECT_STORE_NAME);
+  return store.getAll();
+};
+
+export default dbPromise;
